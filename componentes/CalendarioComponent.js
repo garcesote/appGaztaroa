d4b54d79d2eeff80a6d89e76dee9c0ed2d2e9917
Iñigo Garcia Ugarte <garcia.127407@e.unavarra.es>
2023-04-13 13:13:00 +0200

    Commit #3

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -2,25 +2,28 @@ import React from 'react';
 import { ListItem, Avatar } from '@rneui/themed'; 
 import { SafeAreaView, FlatList } from 'react-native'; 
 
+
 function Calendario(props) { 
     const renderCalendarioItem = ({item, index}) => { 
         
         return ( 
         
-            <ListItem key={index} bottomDivider> 
+            <ListItem key={index} onPress={() => props.onPress(item.id)} bottomDivider> 
                 <Avatar source={require('./imagenes/40Años.png')} /> 
                 <ListItem.Content> 
                     <ListItem.Title>{item.nombre}</ListItem.Title> 
                     <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle> 
                 </ListItem.Content> 
-            </ListItem> ); 
+            </ListItem> 
+        ); 
 
-        }; 
+    }; 
     
-        return ( 
-            <SafeAreaView> 
-                <FlatList data={props.excursiones} renderItem={renderCalendarioItem} keyExtractor={item => item.id.toString()} /> 
-            </SafeAreaView> ); 
+    return ( 
+        <SafeAreaView> 
+            <FlatList data={props.excursiones} renderItem={renderCalendarioItem} keyExtractor={item => item.id.toString()} /> 
+        </SafeAreaView> 
+    ); 
     } 
     
 export default Calendario;
